fe97e8b7dc83fff645f70c34d401f779d4bd10f9
Kadir Taban <kadirtaban08@gmail.com>
2022-03-05 12:57:04 +0300

    add to favs

--- app.js
+++ app.js
@@ -3,15 +3,21 @@ const app = Vue.createApp({
     data(){
         return{
             showBooks:true ,
-            title:"The final ",
-            author:"Brandon Sanderson",
-            age:45
+            books: [
+                { title:'name of the wind', author:'patrick rothfuss', img:'assets/1.jpeg', isFav:true },
+                { title:'the way of kings', author:'brandon sanderson', img:'assets/2.jpeg', isFav:false },
+                { title:'the final empire', author:'brandon sanderson', img:'assets/3.jpeg', isFav:true },
+            ]
         }
     },
     methods: {
         toggleShowBooks(){
             this.showBooks = !this.showBooks
+        },
+        toggleFav(book){
+            book.isFav = !book.isFav
         }
+        
     }
 })
 
