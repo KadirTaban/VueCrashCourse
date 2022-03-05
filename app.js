4818563b416f1d1141393ad637118decb5cf4240
Kadir Taban <kadirtaban08@gmail.com>
2022-03-05 10:39:14 +0300

    conditional rendering

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const app = Vue.createApp({
 
     data(){
         return{
-
+            showBooks:true ,
             title:"The final ",
             author:"Brandon Sanderson",
             age:45
+        }
+    },
+    methods: {
+        toggleShowBooks(){
+            this.showBooks = !this.showBooks
         }
     }
 })
