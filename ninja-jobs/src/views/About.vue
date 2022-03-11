05ef476f6d52bc247313b6fd053cf73fcb62b5fa
Kadir Taban <kadirtaban08@gmail.com>
2022-03-11 14:16:57 +0300

    ninja jobs

---
+++ ninja-jobs/src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum vero perspiciatis laboriosam ratione praesentium? Consequatur, suscipit, expedita fugiat dolorum ratione quaerat minus voluptas consequuntur deleniti labore neque voluptates aliquid maiores ea necessitatibus ut nemo ipsam modi sequi? Debitis numquam fugit pariatur harum ut at voluptatum praesentium neque explicabo, iste, non aut? Mollitia qui dicta beatae ipsum quisquam totam magni sed deleniti suscipit reiciendis libero quia velit laudantium assumenda sapiente asperiores veniam error labore perferendis consectetur inventore, impedit perspiciatis. Reiciendis non omnis ex nobis dicta voluptate. Maiores, eaque harum fuga fugit aperiam sunt pariatur consectetur amet, perspiciatis totam ut at dicta?</p>
+
+  </div>
+</template>
